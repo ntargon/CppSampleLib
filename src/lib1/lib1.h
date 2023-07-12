55109bfb3d461ed8a49ca5f4b4e1350fdf7fb646
ntargon <argon19951128@gmail.com>
2023-07-12 22:36:50 +0900

    init

---
+++ src/lib1/lib1.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int lib1_func(void);
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include "func.h"
+#include "lib1/lib1.h"
 
 int main(void)
 {
+
+    int val = add(lib1_func(), 2);
+
     std::cout<< "hello world\n";
+    std::cout<< "add(1, 2) = " << val << "\n";
 
     return 0;
 }
